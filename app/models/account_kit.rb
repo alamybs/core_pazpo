528e12778c3a02e0e063dbe0aa707f9f1ff5a63a
alamybs <namakukingkong@gmail.com>
2017-04-19 15:52:48 +0700

    Remove http ssl fix

--- app/models/account_kit.rb
+++ app/models/account_kit.rb
@@ -31,12 +31,14 @@ class AccountKit
 
   def access_user
     if :access_token
-      params   = {
+      params       = {
         access_token: @access_token,
       }
-      uri      = URI.parse("https://graph.accountkit.com/v1.2/me/?access_token=#{params[:access_token]}")
-      request  = Net::HTTP::Get.new(uri)
-      response = http.request(request)
+      uri          = URI.parse("https://graph.accountkit.com/v1.2/me/?access_token=#{params[:access_token]}")
+      http         = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request      = Net::HTTP::Get.new(uri)
+      response     = http.request(request)
       if response.code.eql?("200")
         @users   = JSON.parse(response.body)
         @success = true
@@ -49,14 +51,16 @@ class AccountKit
   end
 
   def access_token
-    params   = {
+    params       = {
       authorization_code: @authorization_code,
       facebook_app_id:    $fb[:facebook_app_id],
       app_secret:         $fb[:facebook_app_secret]
     }
-    uri      = URI.parse("https://graph.accountkit.com/v1.2/access_token?grant_type=authorization_code&code=#{params[:authorization_code]}&access_token=AA|#{params[:facebook_app_id]}|#{params[:app_secret]}")
-    request  = Net::HTTP::Get.new(uri)
-    response = http.request(request)
+    uri          = URI.parse("https://graph.accountkit.com/v1.2/access_token?grant_type=authorization_code&code=#{params[:authorization_code]}&access_token=AA|#{params[:facebook_app_id]}|#{params[:app_secret]}")
+    http         = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request      = Net::HTTP::Get.new(uri)
+    response     = http.request(request)
     if response.code.eql?("200")
       @access_token = JSON.parse(response.body)
       @success      = true
