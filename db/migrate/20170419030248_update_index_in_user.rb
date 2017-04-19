61b9c9ef75ad4e0601b7db1b057bd0a9e169a41c
alamybs <namakukingkong@gmail.com>
2017-04-19 15:05:18 +0700

    Fix migration!

--- db/migrate/20170419030248_update_index_in_user.rb
+++ db/migrate/20170419030248_update_index_in_user.rb
@@ -2,7 +2,6 @@ class UpdateIndexInUser < ActiveRecord::Migration[5.0]
   def change
     remove_index(:users, :name => 'index_users_on_authentication_token_and_email')
 
-    add_index :users, :account_kit_id, unique: true
     add_index :users, :authentication_token, unique: true
     add_index :users, :email, unique: true
   end
